10a4768755b143ffed9d3e801f5dd15bc86613bc
oresho <somuyiwaoreoluwa@gmail.com>
2022-06-25 14:37:39 +0100

    EFBasics

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EntityFrameworkBasics.Data
+{
+    //The database representational model for our application
+    public class ApplicationDbContext : DbContext
+    {
+        #region Public Properties
+        public DbSet<SettingsDataModel> Settings { get; set; }
+        #endregion
+
+        public ApplicationDbContext(DbContextOptions options) // PASSING IN DBCONTEXTOPTIONS AS AN ARGUMENT FOR DB CREATION SO YOU CAN USE DI IN THE STARTUP
+                : base(options)
+        {
+        }
+
+        public ApplicationDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)//configure db
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=Settings;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)//where you define relationships between tables public key etc
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
---
+++ Startup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityFrameworkBasics.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EntityFrameworkBasics
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.Database.EnsureCreated();
+
+                if (!context.Settings.Any()) //ATTEMPTED TO USE BEAN CONFIGURATION, GAVE NULL ID ERROR? DONT KNOW WHY
+                {
+                    context.Settings.Add(new SettingsDataModel
+                    {
+                        Name = "BackgroundColor",
+                        Value = "Red"
+                    });
+                }
+                var settingsLocally = context.Settings.Local.Count();
+                var settingsDatabase = context.Settings.Count();
+
+                var firstLocal = context.Settings.Local.FirstOrDefault();
+                var firstDatabase = context.Settings.FirstOrDefault();
+
+                context.SaveChanges();
+                settingsLocally = context.Settings.Local.Count();
+                settingsDatabase = context.Settings.Count();
+
+                firstLocal = context.Settings.Local.FirstOrDefault();
+                firstDatabase = context.Settings.FirstOrDefault();
+
+            }
+
+
+
+            services.AddControllersWithViews();
+            //services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
